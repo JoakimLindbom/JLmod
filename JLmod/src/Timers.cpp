cda8a4ef75864c32ba5c80115d459e527b32ab00
Joakim Lindbom <Joakim.Lindbom@gmail.com>
2018-01-10 00:38:18 +0100

    First working version

--- JLmod/src/Timers.cpp
+++ JLmod/src/Timers.cpp
@@ -13,8 +13,12 @@ Thanks to Strum for the display widget!
 */
 
 struct Timer {
+    bool running = false;
     float seconds = 0.0f;
-    float counter = 0.0f;
+    float counter = 20.0f;
+    bool trigger = false;
+    float output = 0.0f;
+
     SchmittTrigger resetTrigger;
     Timer() {
         resetTrigger.setThresholds(0.0, 0.01);
@@ -24,24 +28,62 @@ struct Timer {
     }
     void setReset(float reset) {
         if (resetTrigger.process(reset)) {
-            counter = seconds;  //
+            Start();
         }
     }
+
+    void Start() {
+        //rack::debug("RESET!");
+        counter = seconds;  //
+        running = true;
+        output = 0.0f;
+    }
+
+    void ResetOutput() {
+        output = 0.0f;
+    }
+
+
     void step(float dt) {
         counter -= dt;
+        //rack::debug("counter %f", counter);
+
+        if (counter <= 0.0f) {
+            trigger = true;
+            running = false;
+            //setReset(0.0f);
+            if (false) { // TODO: Check interval/once switch
+                Start();
+                //counter = seconds;
+                //running = true;
+            }
+        }
+
+        if (trigger) {
+            output = 12.0f;
+            //resetLight = 1.0;
+            //pulseLight = 1.0;
+            trigger = false;
+        }
+        else {
+            output = 0.0f;
+        }
     }
 
     float light() {
-        return counter<=0?5:0;  // TODO: Check expected level
+        return counter<=0.0f?5.0f:0.0f;  // TODO: Check expected level
     }
 };
 
 
 struct Timers: Module {
     enum ParamIds {
-        timer1_dsp_PARAM,
-        timer2_dsp_PARAM,
-        timer3_dsp_PARAM,
+        timer1_PARAM,
+        timer1_RESET_BUTTON,
+        timer2_PARAM,
+        timer2_RESET_BUTTON,
+        timer3_PARAM,
+        timer3_RESET_BUTTON,
         NUM_PARAMS
     };
     enum InputIds {
@@ -116,21 +158,53 @@ struct SmallIntegerDisplayWidgeter : TransparentWidget {
 
 void Timers::step() {
 
-    timer1_dsp_val = params[timer1_dsp_PARAM].value;
-    outputs[TRIGGER1_OUTPUT].value = 0.0f;
+    timer1_dsp_val = params[timer1_PARAM].value;
+    timer1.setTime(timer1_dsp_val);
 
-    timer1.step(1.0 / engineGetSampleRate());
-    counter1_val = timer1.counter;
+    if (params[timer1_RESET_BUTTON].value > 0.0f || inputs[RESET1_INPUT].value > 0.0f) {
+        timer1.Start();
+    }
 
-    timer2_dsp_val = params[timer2_dsp_PARAM].value;
-    outputs[CV2_INPUT].value = 0.0f;
+    if (timer1.running) {
+        timer1.step(1.0 / engineGetSampleRate());
+        counter1_val = timer1.counter;
+    }
+    else {
+        timer1.ResetOutput();
+    }
+    outputs[TRIGGER1_OUTPUT].value = timer1.output;
 
-    counter2_val = 4.0f;  // TODO: implement counter
+    timer2_dsp_val = params[timer2_PARAM].value;
+    timer2.setTime(timer2_dsp_val);
 
-    timer3_dsp_val = params[timer3_dsp_PARAM].value;
-    outputs[CV3_INPUT].value = 0.0f;
+    if (params[timer2_RESET_BUTTON].value > 0.0f || inputs[RESET2_INPUT].value > 0.0f) {
+        timer2.Start();
+    }
 
-    counter3_val = 6.0f;  // TODO: implement counter
+    if (timer2.running) {
+        timer2.step(1.0 / engineGetSampleRate());
+        counter2_val = timer2.counter;
+    }
+    else {
+        timer2.ResetOutput();
+    }
+    outputs[TRIGGER2_OUTPUT].value = timer2.output;
+
+    timer3_dsp_val = params[timer3_PARAM].value;
+    timer3.setTime(timer3_dsp_val);
+
+    if (params[timer3_RESET_BUTTON].value > 0.0f || inputs[RESET3_INPUT].value > 0.0f) {
+        timer3.Start();
+    }
+
+    if (timer3.running) {
+        timer3.step(1.0 / engineGetSampleRate());
+        counter3_val = timer3.counter;
+    }
+    else {
+        timer3.ResetOutput();
+    }
+    outputs[TRIGGER3_OUTPUT].value = timer3.output;
 
 }
 
@@ -166,7 +240,8 @@ TimersWidget::TimersWidget() {
 
     addInput(createInput<PJ301MPort>(Vec(8, 123), module, Timers::RESET1_INPUT));
     addInput(createInput<PJ301MPort>(Vec(33, 123), module, Timers::CV1_INPUT));
-    addParam(createParam<RoundBlackKnob>(Vec(58, 95), module, Timers::timer1_dsp_PARAM, 0.0, 1000.0, 0.0));
+    addParam(createParam<LEDButton>(Vec(44, 123), module, Timers::timer1_RESET_BUTTON, 0.0, 1.0, 0.0));
+    addParam(createParam<RoundBlackKnob>(Vec(58, 95), module, Timers::timer1_PARAM, 0.0, 1000.0, 0.0));
     addOutput(createOutput<PJ301MPort>(Vec(113, 123), module, Timers::TRIGGER1_OUTPUT));
 
     // Timer 2
@@ -182,9 +257,10 @@ TimersWidget::TimersWidget() {
     counter2->value = &module->counter2_val;
     addChild(counter2);
 
-    addInput(createInput<PJ301MPort>(Vec(35, 223), module, Timers::RESET2_INPUT));
+    addInput(createInput<PJ301MPort>(Vec(8, 223), module, Timers::RESET2_INPUT));
     addInput(createInput<PJ301MPort>(Vec(33, 223), module, Timers::CV2_INPUT));
-    addParam(createParam<RoundBlackKnob>(Vec(58, 195), module, Timers::timer2_dsp_PARAM, 0.0, 1000.0, 0.0));
+    addParam(createParam<LEDButton>(Vec(44, 223), module, Timers::timer2_RESET_BUTTON, 0.0, 1.0, 0.0));
+    addParam(createParam<RoundBlackKnob>(Vec(58, 195), module, Timers::timer2_PARAM, 0.0, 1000.0, 0.0));
     addOutput(createOutput<PJ301MPort>(Vec(95, 223), module, Timers::TRIGGER2_OUTPUT));
 
     // Timer 3
@@ -200,9 +276,10 @@ TimersWidget::TimersWidget() {
     counter3->value = &module->counter3_val;
     addChild(counter3);
 
-    addInput(createInput<PJ301MPort>(Vec(35, 323), module, Timers::RESET3_INPUT));
+    addInput(createInput<PJ301MPort>(Vec(8, 323), module, Timers::RESET3_INPUT));
     addInput(createInput<PJ301MPort>(Vec(33, 323), module, Timers::CV3_INPUT));
-    addParam(createParam<RoundBlackKnob>(Vec(58, 295), module, Timers::timer3_dsp_PARAM, 0.0, 1000.0, 0.0));
+    addParam(createParam<LEDButton>(Vec(44, 323), module, Timers::timer3_RESET_BUTTON, 0.0, 1.0, 0.0));
+    addParam(createParam<RoundBlackKnob>(Vec(58, 295), module, Timers::timer3_PARAM, 0.0, 1000.0, 0.0));
     addOutput(createOutput<PJ301MPort>(Vec(95, 323), module, Timers::TRIGGER3_OUTPUT));
 
 }
