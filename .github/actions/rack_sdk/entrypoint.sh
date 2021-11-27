5383bd6f751b1bd73b715a23e86160b3aaa49df5
JoakimLindbom <Joakim.Lindbom@gmail.com>
2021-11-27 16:49:06 +0100

    Hard coded SDK

--- .github/actions/rack_sdk/entrypoint.sh
+++ .github/actions/rack_sdk/entrypoint.sh
@@ -3,18 +3,18 @@
 # Expects RACK_SDK_VERSION environment variable to be set in workflow
 
 set -eu
-RACK_SDK_VERSION=2.beta.1
-ARCH=lin
+RACK_SDK_VERSION='2.beta.1'
+ARCH='lin'
 
+echo Testing act
+#echo $RACK_SDK_VERSION
 
-echo ${{ RACK_SDK_VERSION }}
+#curl -o RackSDK.zip https://vcvrack.com/downloads/Rack-SDK-${{ RACK_SDK_VERSION }}-${{ ARCH }}.zip
+#unzip Rack-SDK-${{ RACK_SDK_VERSION }}.zip
+#rm Rack-SDK-${{ RACK_SDK_VERSION }}.zip
 
-curl -o RackSDK.zip https://vcvrack.com/downloads/Rack-SDK-${{ RACK_SDK_VERSION }}-${{ ARCH }}.zip
-unzip Rack-SDK-${{ RACK_SDK_VERSION }}.zip
-rm Rack-SDK-${{ RACK_SDK_VERSION }}.zip
-
-#curl -L https://vcvrack.com/downloads/Rack-SDK-${RACK_SDK_VERSION}.zip -o rack-sdk.zip
-#unzip -o rack-sdk.zip
-#rm rack-sdk.zip
+curl -L https://vcvrack.com/downloads/Rack-SDK-2.beta.1.zip -o rack-sdk.zip
+unzip -o rack-sdk.zip
+rm rack-sdk.zip
 
 mkdir -p plugins
