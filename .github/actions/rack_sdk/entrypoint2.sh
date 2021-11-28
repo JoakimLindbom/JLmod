539e0ac14054bc87a71eed899ef28cab820fb711
JoakimLindbom <Joakim.Lindbom@gmail.com>
2021-11-28 11:15:53 +0100

    Changing Dockerfile

--- .github/actions/rack_sdk/entrypoint2.sh
+++ .github/actions/rack_sdk/entrypoint2.sh
@@ -6,7 +6,9 @@ set -eu
 RACK_SDK_VERSION='2.beta.1'
 ARCH='lin'
 
-echo Testing act
+# export RACK_DIR=${GITHUB_WORKSPACE}/Rack-SDK
+
+echo Testing act 1234
 #echo $RACK_SDK_VERSION
 
 #curl -o RackSDK.zip https://vcvrack.com/downloads/Rack-SDK-${{ RACK_SDK_VERSION }}-${{ ARCH }}.zip
@@ -14,7 +16,11 @@ echo Testing act
 #rm Rack-SDK-${{ RACK_SDK_VERSION }}.zip
 
 curl -L https://vcvrack.com/downloads/Rack-SDK-2.beta.1-lin.zip -o Rack-SDK-2-lin.zip
-unzip -o Rack-SDK-2-lin.zip
+unzip -o -d Rack-SDK-lin Rack-SDK-2-lin.zip
 rm Rack-SDK-2-lin.zip
+
+curl -L https://vcvrack.com/downloads/Rack-SDK-2.beta.1-win.zip -o Rack-SDK-2-win.zip
+unzip -o -d Rack-SDK-win Rack-SDK-2-win.zip
+rm Rack-SDK-2-win.zip
 
 mkdir -p plugins
