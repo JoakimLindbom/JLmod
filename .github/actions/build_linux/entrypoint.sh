61f023a739ca3315a898d03c5746077e063bc066
JoakimLindbom <Joakim.Lindbom@gmail.com>
2021-12-13 08:16:58 +0100

    Changed build scripts

--- .github/actions/build_linux/entrypoint.sh
+++ .github/actions/build_linux/entrypoint.sh
@@ -2,18 +2,13 @@
 
 set -eu
 
-#export RACK_DIR=${GITHUB_WORKSPACE}/Rack-SDK-lin
 export RACK_DIR=${GITHUB_WORKSPACE}/Rack-SDK
-#export RACK_DIR=${GITHUB_WORKSPACE}/Rack-SDK-lin/Rack-SDK
 export RACK_USER_DIR=${GITHUB_WORKSPACE}
 
 echo Download SDK
 pwd
-#cd $RACK_DIR
-#curl -L https://vcvrack.com/downloads/Rack-SDK-"${RACK_SDK_VERSION}"-lin.zip -o rack-sdk.zip
 curl -L https://vcvrack.com/downloads/Rack-SDK-2.0.3-lin.zip -o rack-sdk.zip
 unzip -o rack-sdk.zip
-#unzip -o -d Rack-SDK-lin rack-sdk.zip
 rm rack-sdk.zip
 
 #cd $GITHUB_WORKSPACE
